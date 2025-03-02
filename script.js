3c2d54e97eef8f663a2c3fb92624056efa1fc41d
Nehavarshine2003 <151645966+Nehavarshine2003@users.noreply.github.com>
2025-03-02 17:40:26 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,2 +1,15 @@
 
-console.log("Flowers for you! 💐");
+ // Add the text dynamically
+    const message = document.createElement("p");
+    message.textContent = 'I miss you alotttttt today, and I hope "That girl" (Neha) made you smile today (˘³˘) 💕.';
+    message.style.fontSize = '1.5rem';
+    message.style.color = '#333';
+    message.style.marginTop = '20px';
+    message.style.textAlign = 'center';
+    document.body.appendChild(message);
+
+    // Remove the loading class
+    document.body.classList.remove("not-loaded");
+    clearTimeout(c);
+  }, 1000);
+};
