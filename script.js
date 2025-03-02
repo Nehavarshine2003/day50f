843ac9c2e5aefac7b10d138515a596a7cc7653ee
Nehavarshine2003 <151645966+Nehavarshine2003@users.noreply.github.com>
2025-03-02 17:42:08 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,5 @@
-
- // Add the text dynamically
-    const message = document.createElement("p");
-    message.textContent = 'I miss you alotttttt today, and I hope "That girl" (Neha) made you smile today (˘³˘) 💕.';
-    message.style.fontSize = '1.5rem';
-    message.style.color = '#333';
-    message.style.marginTop = '20px';
-    message.style.textAlign = 'center';
-    document.body.appendChild(message);
-
-    // Remove the loading class
+onload = () => {
+  const c = setTimeout(() => {
     document.body.classList.remove("not-loaded");
     clearTimeout(c);
   }, 1000);
